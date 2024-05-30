e1471574dc509e327eef03f6b2a2634199806ca0
Kim-jaeyeon <jy020507@naver.com>
2024-05-31 00:05:43 +0900

    [Feat] 아파트 조회하기 API 연결과 최신순, 랭킹순 정렬하기

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,22 +3,23 @@ import "./Post.css";
 import chatIcon from "./assets/chat.svg"; // chat.svg 파일을 import
 import heartIcon from "./assets/heart.svg"; // heart.svg 파일을 import
 import fillHeartIcon from "./assets/fillHeart.svg"; // fillHeart.svg 파일을 import
+import axios from "axios";
 // import postData from "./JSON/post.json";
 
 /**
  * 좋아요 수 댓글 수를 관리합니다.
  */
 
-function Post({ nickname, aptname, heart, comment }) {   //postList 컴포넌트에서 json props를 받아온다.
+function Post({ key, id, nickname, aptname, isLiked, likeCount, commentCount }) {   //postList 컴포넌트에서 json props를 받아온다.
   // isHeartClicked 상태 변수와 setIsHeartClicked 함수: 하트 아이콘 클릭 여부를 관리
-  const [isHeartClicked, setIsHeartClicked] = useState(false);
+  const [isHeartClicked, setIsHeartClicked] = useState(isLiked);
   // heart 상태 변수와 setHeart 함수: 하트 숫자를 관리
-  const [heartNum, setHeartNum] = useState(heart);     //좋아요 수 개수
+  const [heartNum, setHeartNum] = useState(likeCount);     //좋아요 수 개수
 //처음 렌더링될 때 해당 게시글의 현재 좋아요 개수를 보여주기 위해 0에서 heart로 변경
   const [commentSelected, setCommentSelected] = useState(false);    //댓글 이모티콘이 선택 됐는지 아닌지
   const [InputValue, setInputValue] = useState(""); 
   const inputCommentRef = useRef(null); 
-  const [commentText, setCommentText] = useState(comment); 
+  const [commentText, setCommentText] = useState([]); 
 
 
   const toggleCommentSelected = () => { //댓글 버튼 선택 boolean을 바꿔주는 메소드
@@ -28,6 +29,7 @@ function Post({ nickname, aptname, heart, comment }) {   //postList 컴포넌트
   };
 
   const onClickCommentAdd = () => {
+
     const newComment = inputCommentRef.current.value;   //인풋칸에 새로 넣은 값
     setCommentText([...commentText, newComment]);   //댓글을 배열에 차곡차곡 저장
     comment.push(newComment);                       //json에 새로운 댓글을 push
@@ -41,16 +43,16 @@ function Post({ nickname, aptname, heart, comment }) {   //postList 컴포넌트
 
   // 하트 아이콘을 클릭했을 때 호출되는 함수
   const toggleHeart = () => {
+    
 
     // 하트가 이미 클릭된 상태라면
     if (isHeartClicked) {
       // 하트 숫자를 1 감소
       setHeartNum(heartNum - 1);
-      heart -= 1;
+      
     } else {
       // 하트 숫자를 1 증가
       setHeartNum(heartNum + 1);
-      heart += 1;
     }
 
     // 하트 클릭 상태를 반전시킴 (true -> false, false -> true)
@@ -82,18 +84,18 @@ function Post({ nickname, aptname, heart, comment }) {   //postList 컴포넌트
             className="icon" // 인라인 스타일 대신 className 사용
           />
           {/* 현재 댓글 숫자를 표시 */}
-          <p id='countComment'>{commentText.length}</p>
+          <p id='countComment'>{commentCount}</p>
         </div>
         {commentSelected &&
-          <>
+          <div id="comment_div">
             <div id='input_comment_div'>
               <input ref={inputCommentRef} value={InputValue} onChange={onChangeInput} type='text' placeholder='댓글을 입력하세요.' id='input_comment'/>
               <button onClick={onClickCommentAdd} >댓글 추가</button>
             </div>
             <div id='comment_list'>
-              {comment.map((comment, i) => <p key={i} ><b>익명{i + 1} </b>{comment}</p>)}
+              {/* {comment.map((comment, i) => <p key={i} ><b>익명{i + 1} </b>{comment}</p>)} */}
             </div>
-          </>
+          </div>
 
         }
       </div>
--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,57 +1,83 @@
 
 import Post from './Post'
 import PostForm from './PostForm';
-import { useState } from 'react';
+import { useState,useEffect} from 'react';
 import './PostList.css';
+import axios from 'axios';
 /**
  * 게시글 등록 폼, 게시글들을 관리합니다. 
  */
 
 // 게시글 등록 함수
-function PostList() {
+function PostList({sortURL}) {    //API URL을 app.jsx로부터 받아옴
   const [posts, setPosts] = useState([]); //게시글 리스트 상태 - 초기값을 빈 배열로 
 
-  /**
-   * 등록 폼 설정 함수 아파트 이름, 비번을 받으면,
-   *  이름 생성 함수가 그 폼에대한 닉네임을 만든다.
-   */
-  const registerPost = (aptName, password) => {
-    const generateUserName = () => {
-      const randomNumber = Math.floor(Math.random() * 1000);
-      return `아기사자${randomNumber}`;
+  // /**
+  //  * 등록 폼 설정 함수 아파트 이름, 비번을 받으면,
+  //  *  이름 생성 함수가 그 폼에대한 닉네임을 만든다.
+  //  */
+  // const registerPost = (aptName, password) => {
+  //   const generateUserName = () => {
+  //     const randomNumber = Math.floor(Math.random() * 1000);
+  //     return `아기사자${randomNumber}`;
+  //   };
+
+  //   // 새로운 게시글 데이터를 저장하기위한 객체
+  //   // 각 게시물 구분을 위한 고유ID
+  //   // 닉네임, 아파트 이름, 공감 수, 댓글들을 관리.
+  //   const newPost = {
+  //     id: Date.now(),// 고유 ID를 사용하여 각 게시물을 구분
+  //     nickname: generateUserName(), // 랜덤 익명 닉네임 생성
+  //     aptname: aptName,
+  //     heart: 0, // 새로운 게시글의 초기 좋아요 개수는 0으로 설정
+  //     comment: [], // 새로운 게시글의 초기 댓글 리스트는 빈 배열로 설정
+  //   };
+  //   setPosts([newPost, ...posts]); 
+  //   // 새로운 게시글을 이전의 게시글 리스트 앞에 추가하는 상태 업데이트 함수이다. 
+  // };
+
+
+
+  useEffect(() => {                     //api에서 가져온 게시물 리스트들이 변할 때마다 리렌더링!
+    const fetchData = async () => {
+      try {
+        const config = {
+          method: 'get',
+          maxBodyLength: Infinity,
+          url: `${sortURL}`,            //최신순을 클릭했는지, 랭킹순을 클릭했는지에 따라서 API url 주소가 바뀝니다!
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+        const response = await axios.request(config);
+        setPosts(response.data.data); // posts를 api에서 가져온 데이터로 설정
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    // 새로운 게시글 데이터를 저장하기위한 객체
-    // 각 게시물 구분을 위한 고유ID
-    // 닉네임, 아파트 이름, 공감 수, 댓글들을 관리.
-    const newPost = {
-      id: Date.now(),// 고유 ID를 사용하여 각 게시물을 구분
-      nickname: generateUserName(), // 랜덤 익명 닉네임 생성
-      aptname: aptName,
-      heart: 0, // 새로운 게시글의 초기 좋아요 개수는 0으로 설정
-      comment: [], // 새로운 게시글의 초기 댓글 리스트는 빈 배열로 설정
-    };
-    setPosts([newPost, ...posts]); 
-    // 새로운 게시글을 이전의 게시글 리스트 앞에 추가하는 상태 업데이트 함수이다. 
-  };
+    fetchData();
+  }, [posts]);    //posts를 관찰!
 
 
   // 게시물들을 렌더링 하는 하는 부분이다. 
   return (
     <div id='PostList_div'>
       <div className="fixed-form-container">
-        <PostForm registerPost={registerPost} />
+        <PostForm  />
         {/* // PostForm 컴포넌트에 registerPost 함수 전달 */}
       </div>
       <div className="posts-container">
-        {posts.map((post) =>
+        {posts.map((post) =>    //api에서 가져온 데이터
           <Post
-            key={post.id}  // React에서 리스트 항목을 고유하게 식별하기 위해 사용 --> 게시글의 댓글, 공감이 독립적으로 관리되도록한다.
-            id={post.id} // 컴포넌트에 post의 ID 값을 전달하여 게시물을 식별하고 관련 데이터를 처리
+            key={post.apartmentName._id}  // React에서 리스트 항목을 고유하게 식별하기 위해 사용 --> 게시글의 댓글, 공감이 독립적으로 관리되도록한다.
+            id={post.apartmentName._id} // 컴포넌트에 post의 ID 값을 전달하여 게시물을 식별하고 관련 데이터를 처리
             nickname={post.nickname} //닉네임을 전달
-            aptname={post.aptname} //이피트 이름을 전달
-            heart={post.heart} //공감 수를 전달
-            comment={post.comment} // 초기 댓글 리스트 전달
+            aptname={post.apartmentName.apartmentName} //이피트 이름을 전달
+            isLiked={post.isLiked} //공감이 눌렸지 안 눌렸는지 전달
+            likeCount={post.likeCount}//공감 수 전달
+            commentCount={post.commentCount}//댓글 개수 전달
+            
           />
         )}
       </div>
