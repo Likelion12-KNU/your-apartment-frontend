ff938636a35b5f17a3b05088038906d913b622d7
easysun10 <2jihye10@gmail.com>
2024-05-30 23:07:46 +0900

    [Feat] 좋아요 API 연결

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,22 +1,24 @@
+import React, { useState } from 'react';
+import Post from './Post';
+import postData from './JSON/post.json';
 
-import Post from './Post'
-import postData from './JSON/post.json'
-import { useState } from 'react';
 function PostList() {
-  const [postList,setPostList]=useState([]);
+  const [postList, setPostList] = useState(postData);
 
   return (
-  <div id='PostList_div'>
-    {postData.map((v,i)=>
-    <Post
-    nickname={v.nickname}
-    aptname={v.aptname}
-    heart={v.heart}
-    comment={v.comment}
-    />)}
-  </div>
-    
-  )
+    <div id='PostList_div'>
+      {postList.data.map((v, i) => (
+        <Post
+          key={v._id}  // 고유한 키 prop으로 v.id 사용
+          id={v._id}  // 각 게시물에 고유한 ID 전달
+          nickname={v.nickname}
+          aptname={v.apartmentName.apartmentName}
+          heart={v.likeCount}
+          comment={v.commentCount}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default PostList
+export default PostList;
